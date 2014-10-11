de3f178919d7b4b2be2a65e3bd77ac26b20cab2e
Florian Eckerstorfer <florian@theroadtojoy.at>
2014-10-12 00:52:59 +0200

    Add copyright and license information to the header

--- src/Workflow.php
+++ src/Workflow.php
@@ -1,6 +1,16 @@
 <?php
 
+/**
+ * This file is part of florianeckerstorfer/plum.
+ *
+ * (c) Florian Eckerstorfer <florian@eckerstorfer.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FlorianEc\Plum;
+
 use FlorianEc\Plum\Converter\ConverterInterface;
 use FlorianEc\Plum\Filter\FilterInterface;
 use FlorianEc\Plum\Reader\ReaderInterface;
