5eeea3b05eaef90922cb4335ef6f48321a96e3f9
Florian Eckerstorfer <florian@eckerstorfer.co>
2014-11-18 18:46:41 +0100

    Add missing PHPDoc

--- src/Writer/CsvWriter.php
+++ src/Writer/CsvWriter.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of cocur/plum.
+ *
+ * (c) Florian Eckerstorfer <florian@eckerstorfer.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Cocur\Plum\Writer;
 
 /**
