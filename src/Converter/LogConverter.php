0944e759266ae449851708aa9d6ca6e31dddeef7
Florian Eckerstorfer <florian@eckerstorfer.co>
2015-04-13 11:45:05 +0200

    Add LogConverter

---
+++ src/Converter/LogConverter.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of plumphp/plum.
+ *
+ * (c) Florian Eckerstorfer <florian@eckerstorfer.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plum\Plum\Converter;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * LogConverter
+ *
+ * @package   Plum\Plum\Converter
+ * @author    Florian Eckerstorfer <florian@eckerstorfer.co>
+ * @copyright 2015 Florian Eckerstorfer
+ */
+class LogConverter implements ConverterInterface
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var string
+     */
+    protected $level;
+
+    /**
+     * @var string
+     */
+    protected $message = 'Converting item';
+
+    /**
+     * @param LoggerInterface $logger
+     * @param string          $level
+     */
+    public function __construct(LoggerInterface $logger, $level = 'debug', $message = null)
+    {
+        $this->logger = $logger;
+        $this->level  = $level;
+        if ($message) {
+            $this->message = $message;
+        }
+    }
+
+    /**
+     * @param mixed $item
+     *
+     * @return mixed
+     */
+    public function convert($item)
+    {
+        $this->logger->log($this->level, $this->message, $item);
+
+        return $item;
+    }
+}
---
+++ tests/Converter/LogConverterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of plumphp/plum.
+ *
+ * (c) Florian Eckerstorfer <florian@eckerstorfer.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plum\Plum\Converter;
+
+use \Mockery as m;
+
+/**
+ * LogConverterTest
+ *
+ * @package   Plum\Plum\Converter
+ * @author    Sebastian Göttschkes <sebastian.goettschkes@googlemail.com>
+ * @copyright 2015-2015 Florian Eckerstorfer
+ * @group     unit
+ */
+class LogConverterTest extends \PHPUnit_Framework_TestCase
+{
+    protected $logger;
+
+    public function setUp()
+    {
+        $this->logger = m::mock('Psr\Log\LoggerInterface');
+    }
+
+    /**
+     * @test
+     * @covers Plum\Plum\Converter\LogConverter::convert()
+     */
+    public function convertWithDefaultOptions()
+    {
+        $converter = new LogConverter($this->logger);
+        $item      = ['a' => 'foo', 'b' => 'bar'];
+
+        $this->logger->shouldReceive('log')->with('debug', 'Converting item', $item);
+
+        $converter->convert($item);
+    }
+
+    /**
+     * @test
+     * @covers Plum\Plum\Converter\LogConverter::convert()
+     */
+    public function convertWithOptions()
+    {
+        $converter = new LogConverter($this->logger, 'notice', 'Log Me');
+        $item      = ['a' => 'foo', 'b' => 'bar'];
+
+        $this->logger->shouldReceive('log')->with('notice', 'Log Me', $item);
+
+        $converter->convert($item);
+    }
+}
